7c0fdb353a78e5e46f98d2a6fc9a7c8dfc432130
Moh <Mohofficial11@outlook.com>
2023-12-14 15:26:04 -0600

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <Navbar />
-          <main>
+          <main className='p-5'>
             {children}
           </main>
         </Theme>
