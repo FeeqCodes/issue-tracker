062916938483270b0d39b0225d2d7a4dd99caa15
Moh <Mohofficial11@outlook.com>
2023-12-14 15:09:02 -0600

    Set up Radix-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@radix-ui/themes'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-   <></>
+   <>
+   <div>
+    <Button>New Issue</Button>
+   </div>
+   </>
   )
 }
