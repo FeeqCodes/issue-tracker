cc673b6fd50aadf76c3a999975f6c4168a0611e5
Moh <Mohofficial11@outlook.com>
2023-12-24 04:50:43 -0600

    Add loading skeletons

---
+++ app/issues/loading.tsx
@@ -0,0 +1,50 @@
+import { Table } from '@radix-ui/themes';
+import React from 'react'
+import StatusBadge from '../components/StatusBadge';
+import IssuesActions from './IssuesActions';
+
+// import loading skeleton
+
+
+
+
+const LoadingIssuesPage = () => {
+  return (
+    <div>
+        <IssuesActions />
+        <Table.Root variant="surface">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Status
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                created
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {issues.map((issue) => (
+              <Table.Row key={issue.id}>
+                <Table.Cell>
+                  <Skeleton />
+                  <div className="block md:hidden">
+                    <Skeleton />
+                  </div>
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  <Skeleton />
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  <Skeleton />
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+    </div>
+  );
+}
+
+export default LoadingIssuesPage
