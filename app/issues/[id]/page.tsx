96fcacfe02655f0019d37b45983ce5b0d47572a9
Moh <Mohofficial11@outlook.com>
2023-12-25 08:49:48 -0600

    Add the commit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,18 @@
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 import { StatusBadge } from "@/app/components";
-
-
+import { Pencil2icon} from '@radix-ui/react-icons'
 import delay from "delay";
+import Link from "next/link";
+
+
+
+/**
+ * Todo
+ * install @radix-ui/react-icons
+ */
 
 
 
@@ -13,6 +20,8 @@ import delay from "delay";
 interface Props {
   params: { id: string };
 }
+
+
 
 const IssueDetailPage = async ({ params }: Props) => {
   // if(typeof params.id !== 'number') notFound();
@@ -23,21 +32,29 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   if (!issue) {
     notFound();
-  } 
+  }
 
-  await delay(2000)
+  await delay(2000);
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="2" my="2">
-        <StatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose mt-4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="2" my="2">
+          <StatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose mt-4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
